58198c6e30d470babf615cfb91e0d3a7289b6f46
ST10377293-Unathi-Tshuma <94136513+zerobbreak@users.noreply.github.com>
2026-05-18 23:22:32 +0200

    feat: integrate Electron for desktop application support; add main entry point and update package scripts for development and production workflows

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ const config = defineConfig({
     tsconfigPaths: true,
     dedupe: ['react', 'react-dom'],
   },
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+  },
+  optimizeDeps: {
+    include: ['tslib'],
+  },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 })
 
